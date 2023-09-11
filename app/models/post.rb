a5728ecd10781fcb472960ff19b7570993a59ea5
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-09-11 15:57:06 +0300

    added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+# app/models/post.rb
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait_title
+  
+    private
+  
+    def clickbait_title
+      unless title.present? && title.match?(/(Won't Believe|Secret|Top \d+|Guess)/)
+        errors.add(:title, "must be clickbait-y")
+      end
+    end
+  end
+  
